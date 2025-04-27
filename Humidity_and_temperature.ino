c89db681a5d6509987108b31dae589a0b9ee60ff
Irene Le <89674421+Irenie0109@users.noreply.github.com>
2025-04-27 17:11:47 +1000

    Update Humidity_and_temperature.ino

--- Humidity_and_temperature.ino
+++ Humidity_and_temperature.ino
@@ -1,18 +1,48 @@
-#include <dht.h>
+#include <WiFiNINA.h>
+#include <ThingSpeak.h>
+#include <DHT.h>
 
-dht DHT;
+const char *ssid = "Optus_508297";
+const char *password = "heezegytes22umL";
+const char *server = "api.thingspeak.com";
+WiFiClient client;
 
-#define DHT11_PIN 7
+unsigned long myChannelNumber = 2936793;
+const char *myWriteAPIKey = "RM4NH65UDV0BCAVV";
 
-void setup(){
+DHT dht(2, DHT22);  // DHT11 sensor on pin 2
+
+void setup() {
   Serial.begin(9600);
+  dht.begin();
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+  ThingSpeak.begin(client);
 }
 
-void loop(){
-  int chk = DHT.read11(DHT11_PIN);
-  Serial.print("Temperature = ");
-  Serial.println(DHT.temperature);
-  Serial.print("Humidity = ");
-  Serial.println(DHT.humidity);
-  delay(1000);
+void loop() {
+  float temp = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temp) || isnan(humidity)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  ThingSpeak.setField(1, temp);   // Field 1 for temperature
+  ThingSpeak.setField(2, humidity); // Field 2 for humidity
+
+  int status = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if (status == 200) {
+    Serial.println("Data sent to ThingSpeak");
+  } else {
+    Serial.println("Failed to send data to ThingSpeak");
+  }
+
+  delay(60000);  // Send data every 60 seconds
 }
